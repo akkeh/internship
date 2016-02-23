c233274639182f548bd1b57d8598702b0fd9f41a
akkeh <akkeaccu@msn.com>
2016-02-23 15:20:41 +0100

    working on improvements

--- pitchEst/notebook/descriptors.py
+++ pitchEst/notebook/descriptors.py
@@ -1,175 +1,95 @@
 import os, sys
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 import essentia as ess
 import essentia.standard as esstd
 
-def essDescriptors(dirname, window='hann', M=2048, H=1024, zp=0):
-    # pitch:
-    pYin = esstd.PitchYinFFT(frameSize=M);
-    
-    # declare descriptors:
-    pSal = esstd.PitchSalience()
-    Centr = esstd.Centroid();
-    HFC = esstd.HFC(); 
-    Flux = esstd.Flux();
-    eDur = esstd.EffectiveDuration();
-    logatt = esstd.LogAttackTime();
-    rollOff = esstd.RollOff();
-    specContr = esstd.SpectralContrast(frameSize=M, sampleRate=44100);
-    Inharm = esstd.Inharmonicity();
-    specComp = esstd.SpectralComplexity();
-    tctotot = esstd.TCToTotal();
-    strongP = esstd.StrongPeak();
-    triStim = esstd.Tristimulus();
-    dynComp = esstd.DynamicComplexity();
+import pEstAssess as pa
 
-    # framing and windowing stuff:
-    spec = esstd.Spectrum();
-    win = esstd.Windowing(type=window, zeroPadding=zp);
-    Env = esstd.Envelope();
-    SpecPeaks = esstd.SpectralPeaks();
-    HarmPeaks = esstd.HarmonicPeaks();
+def testImprovement(pool, pFunc, M=1000, argv=''):
+    names = np.append(pool['name'], '')
+    N = len(names)
     
-    pool = ess.Pool()
+    for m in range(M):
+        i = np.random.randint(N)
+        filename = names[i]
+        print "file: "+filename
+        loader = esstd.MonoLoader(filename = './sounds/all/'+filename);
+        x = loader();
+        
+        p, c, ip, ic = pFunc((x, argv))
+        
+        
+        pTag = float(pool['annotated_pitch'][i])
+        pEst = float(pool['lowLevel.pitch.median'][i])
+
+        ipEst = np.median(ip)
+        
+        if np.median(p) != pEst:
+            print "Different estimates:\t", np.median(p), '\t', pEst, '\n'
+        # remove sound from possible next sounds: 
+        names = np.delete(names, i)
+        N = len(names)
+
+def noSilence_pYinFFT(argv):
+    x = argv[0]
+    
+    StrtStop = esstd.StartStopSilence();
+    FC = esstd.FrameCutter(frameSize=2048, hopSize=1024);
+    pYin = esstd.PitchYinFFT(frameSize=1024);
+    
+   
+    frames = [];
+    pitch = []; conf = [];
+
+    fc = FC(x);
+    while len(fc) > 0:
+        frames.append(fc)
+        fc = FC(x);
+   
+    for frame in frames:
+        p, c = pYin(frame)
+        pitch.append(p);
+        conf.append(c);
+        srtStp = StrtStop(frame)
+
+    start, stop = srtStp;
+
+    ns_pitch = pitch[start:stop];
+    ns_conf = conf[start:stop]
+    return pitch, conf, ns_pitch, ns_conf
+
+def improvePitch(dirname):
     # list files:
-    files = 0
     filenames = [];
-    pTag = [];
     print "listing files"
     with open('./results/' + dirname.split('/')[-2] + '_ESS.txt') as f:
         f.readline()    # skip first line
         line = f.readline();
         while line != '':
             filenames.append(line.split('\t')[0].split('/')[-1])
-            pTag.append(float(line.split('\t')[4]));  
-    
             line = f.readline();
-            files += 1 
-
-    print "Found " + str(files) + " files in directory: " + dirname
-    filecount = 0
+    pool = ess.Pool()
 
     for filename in filenames:
-        print "file: " + str(filecount+1) + " / " + str(files)
-        pool.add('names', filename)
-        # load, cut and window the audio:
-        loader = esstd.MonoLoader(filename = dirname+filename)
+        print "file: " + filename
+        loader = esstd.MonoLoader(filename = dirname+filename);
         x = loader();
-        env = Env(x);
-
-        N = len(x)
+        return x
+        t_pool = ess.Pool()
+        pitch, conf = noSilence_pYinFFT(x)
+        t_pool.add('name', filename);
+        t_pool.add('noSilence.pitch.mean', np.mean(pitch));
+        t_pool.add('noSilence.pitch.median', np.median(pitch));
+        t_pool.add('noSilence.conf.mean', np.mean(conf));
+        t_pool.add('noSilence.conf.median', np.median(conf));
         
-        if N / 2 != N / 2.:
-            x = ess.array(np.append(x, np.zeros(1)));
-            N = N+1
+        for dName in t_pool.descriptorNames():
+            pool.add(dName, t_pool[dName])
         
-        
-        fc = esstd.FrameCutter(frameSize=M, hopSize=H);
-        frames = int((N-M) / H);
-
-        # initiate empty arrays:
-        pitch = []; conf = [];
-        psal = []; centroid = []; hfc = []; flux = []; 
-        rolloff = []; speccontr = []; inharm = [];
-        speccomp = []; strongp = []; 
-        tristim0 = []; tristim1 = []; tristim2 = [];
-
-        for m in range(frames):
-            frame = fc(x)
-            mX = spec(win(frame))
-        
-            # pitchYinFFT:
-            pYinPitch, pYinConf = pYin(mX)
-            pitch.append(pYinPitch);
-            conf.append(pYinConf);
-            
-            psal.append(pSal(mX));                              # salience
-            centroid.append(Centr(mX))                          # centroid
-            hfc.append(HFC(mX));                                # dynamic complexity      
-            flux.append(Flux(mX))                               # flux
-            rolloff.append(rollOff(mX));                        # rolloff
-            speccontr.append(specContr(mX));                    # spectral contrast
-            # inharmonicity:
-            specp = SpecPeaks(mX)
-            harmp = HarmPeaks(specp[0][1:], specp[1][1:], pYinPitch);
-            inharm.append(Inharm(harmp[0], harmp[1]));
-            speccomp.append(specComp(mX));                      # spectral complexity
-            strongp.append(strongP(mX));                        # StrongPeak
-            # Tristimulus:
-            tristim = triStim(harmp[0], harmp[1]);
-            tristim0.append(tristim[0]); 
-            tristim1.append(tristim[1]); 
-            tristim2.append(tristim[2]); 
-            
-
-        # pitchYin:
-        pool.add('pYinPitch.mean', np.mean(pitch))
-        pool.add('pYinPitch.median', np.median(pitch));
-        pool.add('pYinPitch.std', np.std(pitch));
-        pool.add('pYinConf.mean', np.mean(conf));
-        pool.add('pYinConf.median', np.median(conf));
-        pool.add('pYinConf.std', np.std(conf));            
-        # Salience:
-        pool.add('salience.mean', np.mean(psal))
-        pool.add('salience.median', np.median(psal))
-        pool.add('salience.std', np.std(psal))
-        # Centroid:
-        pool.add('centroid.mean', np.mean(centroid))
-        pool.add('centroid.median', np.median(centroid))
-        pool.add('centroid.std', np.std(centroid))
-        # HFC:
-        pool.add('hfc.mean', np.mean(hfc))
-        pool.add('hfc.median', np.median(hfc))
-        pool.add('hfc.std', np.std(hfc))
-        # Flux:
-        pool.add('flux.mean', np.mean(flux))
-        pool.add('flux.median', np.median(flux))
-        pool.add('flux.std', np.std(flux))
-        # EffectiveDuration: 
-        pool.add('effectiveduration', eDur(x))
-        # LogAttackTime:
-        pool.add('logattacktime', logatt(env))
-        # RollOff:
-        pool.add('rolloff.mean', np.mean(rolloff))
-        pool.add('rolloff.median', np.median(rolloff))
-        pool.add('rolloff.std', np.std(rolloff))
-        # SpectralContrast:
-        pool.add('specralcontrast.mean', np.mean(speccontr))
-        pool.add('spectralcontrast.median', np.median(speccontr))
-        pool.add('spectralcontrast.std', np.std(speccontr))
-        # Inharmonicity:
-        pool.add('inharmonicity.mean', np.mean(inharm))
-        pool.add('inharmonicity.median', np.median(inharm))
-        pool.add('inharmonicity.std', np.std(inharm))
-        # SpectralComplexity:
-        pool.add('spectralcomplexity.mean', np.mean(speccomp));
-        pool.add('spectralcomplexity.median', np.median(speccomp));
-        pool.add('spectralcomplexity.std', np.std(speccomp));
-        # TCToTotal:
-        pool.add('tctototal', tctotot(env))
-        # StrongPeak
-        pool.add('strongpeak.mean', np.mean(strongp))            
-        pool.add('strongpeak.median', np.median(strongp))            
-        pool.add('strongpeak.std', np.std(strongp))            
-        # Tristimulus
-        pool.add('tristimulus.0.mean', np.mean(tristim0))        
-        pool.add('tristimulus.0.median', np.median(tristim0))        
-        pool.add('tristimulus.0.std', np.std(tristim0))        
-        pool.add('tristimulus.1.mean', np.mean(tristim1))        
-        pool.add('tristimulus.1.median', np.median(tristim1))        
-        pool.add('tristimulus.1.std', np.std(tristim1))        
-        pool.add('tristimulus.2.mean', np.mean(tristim2))        
-        pool.add('tristimulus.2.median', np.median(tristim2))        
-        pool.add('tristimulus.2.std', np.std(tristim2))        
-        # DynamicComplexity:
-        pool.add('dynamiccomplexity', dynComp(x))
-
-        pool.add('ptag', pTag[filecount]);
-        filecount += 1
-    
-    print pool.descriptorNames();
-    return pool
+    return pool        
 
 def essExtractor(dirname):
     Extr = esstd.Extractor(rhythm=False, midLevel=False);
@@ -217,10 +137,12 @@ def calcAll(test=0):
     esstd.YamlOutput(filename = './results/descriptors.json', format='json')(pool)
 
 
-if len(sys.argv) > 1:
+def loadData():
     try:
         dataIn = esstd.YamlInput(filename='./results/descriptors.json')
         pool = dataIn();
     except:
         calcAll()
         pool = dataIn();
+    return pool
+
--- pitchEst/notebook/loadResults.py
+++ pitchEst/notebook/loadResults.py
@@ -46,83 +46,48 @@ carlosJSON =  readData('./results/carlos_JSON.txt')
 goodsoundsJSON =  readData('./results/good-sounds_JSON.txt')
 modularsamplesJSON =  readData('./results/modularsamples_JSON.txt')
 
-csJ_N = len(carlosJSON[0])-1
-gsJ_N = len(goodsoundsJSON[0])-1
-msJ_N = len(modularsamplesJSON[0])-1
+csN = len(carlosJSON[0])-1
+gsN = len(goodsoundsJSON[0])-1
+msN = len(modularsamplesJSON[0])-1
 
 # soundfile-data:
-# Freesound:
-carlosESS =  readData('./results/carlos_ESS.txt')
-goodsoundsESS =  readData('./results/good-sounds_ESS.txt')
-modularsamplesESS =  readData('./results/modularsamples_ESS.txt')
-csE_N = len(carlosESS[0])-1
-gsE_N = len(goodsoundsESS[0])-1
-msE_N = len(modularsamplesESS[0])-1
-
-'''
-carlosESSroll512 = readData('./results/carlos_ESS_roll512.txt')
-goodsoundsESSroll512 = readData('./results/good-sounds_ESS_roll512.txt')
-modularsamplesESSroll512 = readData('./results/modularsamples_ESS_roll512.txt')
-csEr_N = len(carlosESSroll512[0])-1
-gsEr_N = len(goodsoundsESSroll512[0])-1
-msEr_N = len(modularsamplesESSroll512[0])-1
-'''
-
+carlosESS = readData('./results/carlos_ESS.txt')
+goodsoundsESS = readData('./results/good-sounds_ESS.txt')
+modularsamplesESS = readData('./results/modularsamples_ESS.txt')
 # non Freesound:
 philharmoniaESS =  readData('./results/philharmonia_ESS.txt')
 iowaESS =  readData('./results/IOWA_ESS.txt')
-phE_N = len(philharmoniaESS[0])-1
-iwE_N = len(iowaESS[0])-1
-'''
-philharmoniaESSroll512 =  readData('./results/philharmonia_ESS_roll512.txt')
-iowaESSroll512 =  readData('./results/IOWA_ESS_roll512.txt')
-phEr_N = len(philharmoniaESSroll512[0])-1
-iwEr_N = len(iowaESSroll512[0])-1
-'''
+phN = len(philharmoniaESS[0])-1
+iwN = len(iowaESS[0])-1
 
-print "read "+str(csJ_N+gsJ_N+msJ_N)+" lines of Freesound data"
-print "read "+str(csE_N+gsE_N+msE_N)+" lines of Freesound soundfile data"
-print "read "+str(phE_N+iwE_N)+" lines of non-Freesound soundfile data"
+print "read "+str(csN+gsN+msN)+" lines of Freesound data"
+print "read "+str(phN+iwN)+" lines of non-Freesound soundfile data"
 
-print "Total amount of sounds assessed: "+str(csJ_N+gsJ_N+msJ_N + phE_N+iwE_N)
+print "Total amount of sounds assessed: "+str(csN+gsN+msN + phN+iwN)
 
 
 # load all the fields into arrays:
 
-# get all the errors:
-field = 'err'
-csJ_err =  np.array(getField(carlosJSON, field), dtype='float')
-gsJ_err =  np.array(getField(goodsoundsJSON, field), dtype='float')
-msJ_err =  np.array(getField(modularsamplesJSON, field), dtype='float')
+# get all the estimated pitches:
+field = 'pEst'
+csJ_pEst =  np.array(getField(carlosJSON, field), dtype='float32')
+gsJ_pEst =  np.array(getField(goodsoundsJSON, field), dtype='float32')
+msJ_pEst =  np.array(getField(modularsamplesJSON, field), dtype='float32')
 
-csE_err =  np.array(getField(carlosESS, field), dtype='float')
-gsE_err =  np.array(getField(goodsoundsESS, field), dtype='float')
-msE_err =  np.array(getField(modularsamplesESS, field), dtype='float')
-
-phE_err =  np.array(getField(philharmoniaESS, field), dtype='float')
-iwE_err =  np.array(getField(iowaESS, field), dtype='float')
-
-# sample the modularsamples results:
-
-
-# append some error-arrays:
-frsJSON_err = np.append(csJ_err, np.append(gsJ_err, msJ_err));
-frsESS_err = np.append(csE_err, np.append(gsE_err, msE_err));
-ESS_err = np.append(csE_err, np.append(gsE_err, np.append(msE_err, np.append(phE_err, iwE_err))));
-
+frsJSON_pEst = np.append(csJ_pEst, np.append(gsJ_pEst, msJ_pEst));
 
 # get all the annotated MIDI notes:
 field = 'midinote'
-csJ_midi =  np.array(getField(carlosJSON, field), dtype='float')
-gsJ_midi =  np.array(getField(goodsoundsJSON, field), dtype='float')
-msJ_midi =  np.array(getField(modularsamplesJSON, field), dtype='float')
+csJ_midi =  np.array(getField(carlosJSON, field), dtype='float32')
+gsJ_midi =  np.array(getField(goodsoundsJSON, field), dtype='float32')
+msJ_midi =  np.array(getField(modularsamplesJSON, field), dtype='float32')
 
-csE_midi =  np.array(getField(carlosESS, field), dtype='float')
-gsE_midi =  np.array(getField(goodsoundsESS, field), dtype='float')
-msE_midi =  np.array(getField(modularsamplesESS, field), dtype='float')
+csE_midi =  np.array(getField(carlosESS, field), dtype='float32')
+gsE_midi =  np.array(getField(goodsoundsESS, field), dtype='float32')
+msE_midi =  np.array(getField(modularsamplesESS, field), dtype='float32')
 
-phE_midi =  np.array(getField(philharmoniaESS, field), dtype='float')
-iwE_midi =  np.array(getField(iowaESS, field), dtype='float')
+phE_midi =  np.array(getField(philharmoniaESS, field), dtype='float32')
+iwE_midi =  np.array(getField(iowaESS, field), dtype='float32')
 
 # append some midi-arrays:
 frsJSON_midi = np.append(csJ_midi, np.append(gsJ_midi, msJ_midi));
@@ -131,79 +96,28 @@ ESS_midi = np.append(csE_midi, np.append(gsE_midi, np.append(msE_midi, np.append
 
 # get all the annotated pitches:
 field = 'pTag'
-csJ_pTag =  np.array(getField(carlosJSON, field), dtype='float')
-gsJ_pTag =  np.array(getField(goodsoundsJSON, field), dtype='float')
-msJ_pTag =  np.array(getField(modularsamplesJSON, field), dtype='float')
+csJ_pTag =  np.array(getField(carlosJSON, field), dtype='float32')
+gsJ_pTag =  np.array(getField(goodsoundsJSON, field), dtype='float32')
+msJ_pTag =  np.array(getField(modularsamplesJSON, field), dtype='float32')
 
-csE_pTag =  np.array(getField(carlosESS, field), dtype='float')
-gsE_pTag =  np.array(getField(goodsoundsESS, field), dtype='float')
-msE_pTag =  np.array(getField(modularsamplesESS, field), dtype='float')
-
-phE_pTag =  np.array(getField(philharmoniaESS, field), dtype='float')
-iwE_pTag=  np.array(getField(iowaESS, field), dtype='float')
-
-# append some pTag-arrays:
 frsJSON_pTag = np.append(csJ_pTag, np.append(gsJ_pTag, msJ_pTag));
-frsESS_pTag = np.append(csE_pTag, np.append(gsE_pTag, msE_pTag));
-ESS_pTag = np.append(csE_pTag, np.append(gsE_pTag, np.append(msE_pTag, np.append(phE_pTag, iwE_pTag))));
-
-# get all the estimated pitches:
-field = 'pEst'
-csJ_pEst =  np.array(getField(carlosJSON, field), dtype='float')
-gsJ_pEst =  np.array(getField(goodsoundsJSON, field), dtype='float')
-msJ_pEst =  np.array(getField(modularsamplesJSON, field), dtype='float')
-
-csE_pEst =  np.array(getField(carlosESS, field), dtype='float')
-gsE_pEst =  np.array(getField(goodsoundsESS, field), dtype='float')
-msE_pEst =  np.array(getField(modularsamplesESS, field), dtype='float')
-
-phE_pEst =  np.array(getField(philharmoniaESS, field), dtype='float')
-iwE_pEst =  np.array(getField(iowaESS, field), dtype='float')
-
-# append some pEst-arrays:
-frsJSON_pEst = np.append(csJ_pEst, np.append(gsJ_pEst, msJ_pEst));
-frsESS_pEst = np.append(csE_pEst, np.append(gsE_pEst, msE_pEst));
-ESS_pEst = np.append(csE_pEst, np.append(gsE_pEst, np.append(msE_pEst, np.append(phE_pEst, iwE_pEst))));
 
 # get all the confidences:
 field = 'confidence'
-csJ_conf =  np.array(getField(carlosJSON, field), dtype='float')
-gsJ_conf =  np.array(getField(goodsoundsJSON, field), dtype='float')
-msJ_conf =  np.array(getField(modularsamplesJSON, field), dtype='float')
+csJ_conf =  np.array(getField(carlosJSON, field), dtype='float32')
+gsJ_conf =  np.array(getField(goodsoundsJSON, field), dtype='float32')
+msJ_conf =  np.array(getField(modularsamplesJSON, field), dtype='float32')
 
-csE_conf =  np.array(getField(carlosESS, field), dtype='float')
-gsE_conf =  np.array(getField(goodsoundsESS, field), dtype='float')
-msE_conf =  np.array(getField(modularsamplesESS, field), dtype='float')
-
-phE_conf =  np.array(getField(philharmoniaESS, field), dtype='float')
-iwE_conf =  np.array(getField(iowaESS, field), dtype='float')
-
-# append some conf-arrays:
 frsJSON_conf = np.append(csJ_conf, np.append(gsJ_conf, msJ_conf));
-frsESS_conf = np.append(csE_conf, np.append(gsE_conf, msE_conf));
-ESS_conf = np.append(csE_conf, np.append(gsE_conf, np.append(msE_conf, np.append(phE_conf, iwE_conf))));
 
 # get all the saliences:
 field = 'salience'
-csJ_sal =  np.array(getField(carlosJSON, field), dtype='float')
-gsJ_sal =  np.array(getField(goodsoundsJSON, field), dtype='float')
-msJ_sal =  np.array(getField(modularsamplesJSON, field), dtype='float')
+csJ_sal =  np.array(getField(carlosJSON, field), dtype='float32')
+gsJ_sal =  np.array(getField(goodsoundsJSON, field), dtype='float32')
+msJ_sal =  np.array(getField(modularsamplesJSON, field), dtype='float32')
 
-csE_sal =  np.array(getField(carlosESS, field), dtype='float')
-gsE_sal =  np.array(getField(goodsoundsESS, field), dtype='float')
-msE_sal =  np.array(getField(modularsamplesESS, field), dtype='float')
-
-phE_sal =  np.array(getField(philharmoniaESS, field), dtype='float')
-iwE_sal =  np.array(getField(iowaESS, field), dtype='float')
-
-# append some salience-arrays:
 frsJSON_sal = np.append(csJ_sal, np.append(gsJ_sal, msJ_sal));
-frsESS_sal = np.append(csE_sal, np.append(gsE_sal, msE_sal));
-ESS_sal = np.append(csE_sal, np.append(gsE_sal, np.append(msE_sal, np.append(phE_sal, iwE_sal))));
-
-
 
 # get all data from the essentia Extractor:
 from descriptors import *
-print "descriptors: " + pool.descriptorNames()
-
+pool = loadData()
