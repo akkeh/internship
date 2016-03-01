24161ef7021921df2242979c7993f13bfdedd5bf
akkeh <akkeaccu@msn.com>
2016-03-01 12:03:18 +0100

    update ipython notebook

--- pitchEst/notebook/loadResults.py
+++ pitchEst/notebook/loadResults.py
@@ -13,6 +13,19 @@ def readJSONS(dirname):
 
     return pool
 
+def sortPool(static, sort):
+        
+    pool = ess.Pool()
+    for name in static['name']:
+        i = 0;
+        while sort['name'][i] != name[0].split('.ogg')[0]:
+            i+=1
+        for dname in sort.descriptorNames():
+            pool.add(dname, sort[dname][i])
+
+    return pool
+
+
 csN = 470
 gsN = 113
 
@@ -25,39 +38,33 @@ iwN = 639
 #print "Total amount of sounds assessed: "+str(csN+gsN + phN+iwN)
 
 # get all data from the essentia Extractor:
+print "importing functions"
 from descriptors import *
+print "loading data"
 pool = loadData()
-
-
-carlosFrs = readJSONS('./sounds/carlosJson/')
-goodsoundsFrs = readJSONS('./sounds/good-soundsJson/')
-
-frsPool = ess.Pool()
-
-for dname in carlosFrs.descriptorNames():
-    for val in carlosFrs[dname]:
-        frsPool.add(dname, val)
-
-for dname in goodsoundsFrs.descriptorNames():
-    for val in goodsoundsFrs[dname]:
-        frsPool.add(dname, val)
 
 locPool = ess.Pool()
 for dname in pool.descriptorNames():
     for val in pool[dname][:csN+gsN]:
         locPool.add(dname, val)
 
-def sortPool(static, sort):
-        
-    pool = ess.Pool()
-    for name in static['name']:
-        i = 0;
-        while sort['name'][i] != name[0].split('.ogg')[0]:
-            i+=1
-        for dname in sort.descriptorNames():
-            pool.add(dname, sort[dname][i])
+print "loading Freesound data"
+try:
+    frsDataIn = esstd.YamlInput(filename='./results/freesoundData.json')
+    frsPool = frsDataIn();
+except:
+    carlosFrs = readJSONS('./sounds/carlosJson/')
+    goodsoundsFrs = readJSONS('./sounds/good-soundsJson/')
 
-    return pool
+    frsPool = ess.Pool()
+
+    for dname in carlosFrs.descriptorNames():
+        for val in carlosFrs[dname]:
+            frsPool.add(dname, val)
+
+    for dname in goodsoundsFrs.descriptorNames():
+        for val in goodsoundsFrs[dname]:
+            frsPool.add(dname, val)
 
 frsPool = sortPool(locPool, frsPool)
 
