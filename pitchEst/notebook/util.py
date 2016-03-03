2cf8702f4c42de3a5dd00da5ebed3dcd6eefe498
akkeh <akkeaccu@msn.com>
2016-03-03 18:05:21 +0100

    end of day

--- pitchEst/notebook/util.py
+++ pitchEst/notebook/util.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+def normalise(x):
+    if np.max(abs(x)) > 0:
+        return x / np.max(abs(x))
+    else:
+        return x
 
 def freq2midi(freq, fref=440.):
     return np.trunc(np.log2(freq / float(fref)) * 12 + 49)
