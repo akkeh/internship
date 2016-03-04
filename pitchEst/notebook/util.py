a765fbf3238a0324f43c900e4167b9baac787caa
akkeh <akkeaccu@msn.com>
2016-03-04 18:27:21 +0100

    Updating graphs, and adding octave error prediction

--- pitchEst/notebook/util.py
+++ pitchEst/notebook/util.py
@@ -7,4 +7,7 @@ def normalise(x):
         return x
 
 def freq2midi(freq, fref=440.):
-    return np.trunc(np.log2(freq / float(fref)) * 12 + 49)
+    return np.log2(freq / float(fref)) * 12 + 49
+
+def midi2freq(midi, fref=440.):
+    return 2**((midi - 49) / 12.) * fref
