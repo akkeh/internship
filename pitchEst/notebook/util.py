f51810f81b1b19c37d9c9d79603e3cc13e402675
akkeh <akkeaccu@msn.com>
2016-03-10 17:01:51 +0100

    working on descriptor/error correlations (notebook)

--- pitchEst/notebook/util.py
+++ pitchEst/notebook/util.py
@@ -13,3 +13,19 @@ def freq2midi(freq, fref=440.):
 
 def midi2freq(midi, fref=440.):
     return 2**((midi - 49) / 12.) * fref
+
+
+
+def remove_overlap(a, b):
+    '''
+        returns a without overlapping elements from b
+    '''
+    i = 0
+    i_double = np.array([])
+    for aval in a:
+        for bval in b:
+            if aval == bval:
+                i_double = np.append(i_double, i);
+        i+=1
+
+    return np.delete(a, i_double)
